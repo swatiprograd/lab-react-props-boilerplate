79b28ecc28ed6fb9b6ec4f3b8e92b02d679db075
Swati Pandey <swati.p@prograd.org>
2023-08-13 19:46:58 +0530

    Use Class Component

--- src/AppClass.js
+++ src/AppClass.js
@@ -2,7 +2,27 @@ import { Component } from "react";
 import "./App.css"
 import elephant from "./images/elephant.jpeg";
 
-export default class AppClass extends Component{
+export default class AppClass extends Component {
 
   // code here
+
+  render() {
+    const imgData = this.props.data;
+    return (
+      <div>
+        <h1>Kalvium Gallary</h1>
+        <div className="row">
+          {
+            imgData.map((image) => {
+              return (
+                <div className="column" key={image.id}>
+                  <img src={image.img} alt="" />
+                </div>
+              )
+
+            })};
+        </div>
+      </div>
+    );
+  }
 }
