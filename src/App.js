a78a54c719159f84ff1a469bee3d4e197929ebf7
Swati Pandey <swati.p@prograd.org>
2023-08-13 19:40:57 +0530

    Use Functional Component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,26 @@
 import './App.css';
 import elephant from "./images/elephant.jpeg";
 
-const imageData = ()=>{
-  let data = [
-    {
-      id:1,
-      img:elephant
-    },
-    {
-      id:2,
-      img:elephant
-    },
-    {
-      id:3,
-      img:elephant
-    },
-    {
-      id:4,
-      img:elephant
-    }
-  ]
-  return data;
-}
-
-function App() {
+function App(props) {
   // code here
+  const imgData = props.data;
+
+  return (
+    <div>
+      <h1>Kalvium Gallary</h1>
+      <div className="row">
+        {
+          imgData.map((image) => {
+            return (
+              <div className="column" key={image.id}>
+                <img src={image.img} alt="" />
+              </div>
+            )
+
+          })};
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 // import AppClass from "./AppClass";
+import Data from "./components/DataComponent";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />   
+    <App data={Data}/>   
     {/* change the above line - for interchaning between App and AppClass */}
   </React.StrictMode>
 );
